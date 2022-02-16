2a3bbdf923945747c935a9797eacfa0881de0912
Sara Cormican <S_Comican@yahoo.com>
2022-02-16 14:13:24 -0500

    set up login page

---
+++ src/ConnectAuth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJUtjfXojpFuDV1PXCaJnYU2w2w4gh8mQ",
+  authDomain: "fir-authsc.firebaseapp.com",
+  projectId: "fir-authsc",
+  storageBucket: "fir-authsc.appspot.com",
+  messagingSenderId: "107172025563",
+  appId: "1:107172025563:web:d835f18e5180ca71dc28fe"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
--- src/scenes/Signup.js
+++ src/scenes/Signup.js
@@ -1,34 +1,65 @@
-import { useState} from 'react'
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../ConnectAuth";
 
-export default function Signup() {
-    const[email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        alert(`Trying to sign-up as ${email}`);
-    }
-    return (
-        <>
-        <h1>Signup</h1>
-        <hr/>
-        <form onSubmit={handleFormSubmit}>
-            <label>Email: 
-                <input 
-                type='email'
-                value={email} 
-                onChange={e => setEmail(e.target.value)} />
-            </label>
-            <label>Password: 
-                <input 
-                type='password' 
-                value={password} 
-                onChange={e=> setPassword(e.target.value)}/></label>
+export default function Signup({ setUser}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+const navigate = useNavigate(); 
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+      setUser(result.user)
+      navigate('/')
+      })
+      .catch(alert);
+  }; 
+  const handleGoogleLogin = () => {
+signInWithPopup(auth, provider)
+.then((result) => {
+    setUser(result.user)
+    navigate('/')
+    })
+    .catch(alert);
+  }
+  return (
+    <>
+      <h1>Signup</h1>
+      <hr />
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
         <br />
-<input type='submit' value='Sign up'/>
-</form>
-        <p>Already a user? <Link to='login'>Login</Link></p>
-        </>
-    )
-
+        <input type="submit" value="Sign up" />
+      </form>
+      <button 
+      onClick={handleGoogleLogin}
+      style={{
+          backgroundColor: 'black',
+       color: 'white',
+       border: 'none'}}>
+        Sign in with Google</button>
+      
+      <p>  Already a user? <Link to="login">Login</Link>
+      </p>
+    </>
+  );
 }
